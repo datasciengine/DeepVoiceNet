5bb6e7ff95fb22d0ffb0333ffeb932aaf52bccbc
MSAHIN <murat.sahin@normdigital.com>
2023-08-03 09:50:39 +0300

    Delete not required files.

--- test1.py
+++
@@ -1,9 +0,0 @@
-from myspsolution import MyPraatAnalysis
-
-audio_path = "normal_okuma-0.5.wav"
-praat_path = "myspsolution.praat"
-
-mysp = MyPraatAnalysis(sound_path=audio_path,
-                       praat_path=praat_path)
-
-mysp.analyze()
